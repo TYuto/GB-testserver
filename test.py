c770bfee01ec459f7d20663fe5fe0760147a69ff
Yuto Tamura <yuto.tamur@gmail.com>
2019-07-23 16:19:32 +0900

    [fix]bug

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import os
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
